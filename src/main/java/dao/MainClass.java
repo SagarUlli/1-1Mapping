7cc2d07a147d7286f822039af75a7ed5cbeb5d72
SagarUlli <sagarulli5@gmail.com>
2025-03-30 10:21:29 +0530

    mapping

--- src/main/java/dao/MainClass.java
+++ src/main/java/dao/MainClass.java
@@ -14,28 +14,48 @@ public class MainClass {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		EntityTransaction entityTransaction = entityManager.getTransaction();
 
-		entityTransaction.commit();
+		
+		ObjectB b1 = new ObjectB();
+		b1.setPNo("ALUPU1526E");
+		b1.setAddress("Kotturu");
 
-		ObjectB b = new ObjectB();
-		b.setPNo("ALUPU1526E");
-		b.setAddress("Kotturu");
+		ObjectA a1 = new ObjectA();
+		a1.setAge(23);
+		a1.setPName("ulli sagar");
+		a1.setSalary(25000.00);
+		a1.setOb(b1);
+		
+		ObjectB b2 = new ObjectB();
+		b2.setPNo("ALUPU1535E");
+		b2.setAddress("Kotturu");
 
-		ObjectA a = new ObjectA();
-		a.setAge(23);
-		a.setPName("ulli sagar");
-		a.setSalary(25000.00);
-		a.setOb(b);
+		ObjectA a2 = new ObjectA();
+		a2.setAge(32);
+		a2.setPName("ulli shridhar");
+		a2.setSalary(75000.00);
+		a2.setOb(b2);
+		
+				
 
 //		------------------ Persist ------------------
 
 		entityTransaction.begin();
-		entityManager.persist(a);
+		entityManager.persist(a1);
 		entityTransaction.commit();
 
 		entityTransaction.begin();
-		entityManager.persist(b);
+		entityManager.persist(b1);
 		entityTransaction.commit();
 
+		entityTransaction.begin();
+		entityManager.persist(a2);
+		entityTransaction.commit();
+
+		entityTransaction.begin();
+		entityManager.persist(b2);
+		entityTransaction.commit();
+
+		
 //		------------------ Fetch ------------------
 
 		ObjectA fetchA = entityManager.find(ObjectA.class, "ulli sagar");
@@ -43,11 +63,12 @@ public class MainClass {
 
 		ObjectB fetchB = entityManager.find(ObjectB.class, "ALUPU1526E");
 		System.out.println(fetchB);
+		
 
 //		------------------ Update ------------------
 
 		ObjectA updateA = entityManager.find(ObjectA.class, "ulli sagar");
-		updateA.setPName("suprith");
+		updateA.setSalary(25000.00);
 		entityTransaction.begin();
 		entityManager.merge(updateA);
 		entityTransaction.commit();
@@ -60,13 +81,13 @@ public class MainClass {
 
 //		------------------ Delete ------------------
 
-		ObjectA deleteParent = entityManager.find(ObjectA.class, "ulli sagar");
+		ObjectA deleteParent = entityManager.find(ObjectA.class, "ulli shridhar");
 		deleteParent.setOb(null);
 		entityTransaction.begin();
 		entityManager.merge(deleteParent);
 		entityTransaction.commit();
 
-		ObjectB deleteChild = entityManager.find(ObjectB.class, "Bengaluru");
+		ObjectB deleteChild = entityManager.find(ObjectB.class, "ALUPU1526E");
 		entityTransaction.begin();
 		entityManager.remove(deleteChild);
 		entityTransaction.commit();
